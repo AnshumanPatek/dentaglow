03e899a1aeeb2ee7756a489f422173b772b39738
Anshuman Patek <78948391+AnshumanPatek@users.noreply.github.com>
2025-10-14 17:31:16 +0530

    text in center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="flex h-screen items-center justify-center">
         <h1 className="text-4xl font-bold text-center text-bottom" >Denta Glow is coming soon</h1>
       </div>
     </>
