bebc4462b6b0f43c2030d177f5cf1331f3fe8355
Anshuman Patek <78948391+AnshumanPatek@users.noreply.github.com>
2025-10-14 18:33:42 +0530

    initial setup of Shad cn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import {Spinner} from "@/components/ui/spinner"
+
 export default function Home() {
   return (
     <>
       <div className="flex h-screen items-center justify-center">
         <h1 className="text-4xl font-bold text-center text-bottom" >Denta Glow is coming soon</h1>
+        <Spinner/>  
       </div>
     </>
   );
