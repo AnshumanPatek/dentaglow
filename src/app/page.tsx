37fca286e23d7640eb42f29fd7ba79ff30de711e
Anshuman Patek <78948391+AnshumanPatek@users.noreply.github.com>
2025-10-14 19:19:07 +0530

    inital cerk setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
-import {Spinner} from "@/components/ui/spinner"
+import { Spinner } from "@/components/ui/spinner";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignUpButton,
+  SignOutButton,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <>
+    <ClerkProvider>
       <div className="flex h-screen items-center justify-center">
-        <h1 className="text-4xl font-bold text-center text-bottom" >Denta Glow is coming soon</h1>
-        <Spinner/>  
+        <h1 className="text-4xl font-bold text-center text-bottom">
+          Denta Glow is coming soon
+        </h1>
+        <Spinner />
+
+        <div>
+          <SignedOut>
+            <SignUpButton mode="modal" />
+          </SignedOut>
+        </div>
+
+        <div>
+          <SignedIn>
+            <SignOutButton />
+          </SignedIn>
+        </div>
       </div>
-    </>
+    </ClerkProvider>
   );
 }
