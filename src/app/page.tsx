2e28a15d43e6f2e1a1883054fe78ebee38506c39
Anshuman Patek <78948391+AnshumanPatek@users.noreply.github.com>
2025-10-24 21:56:35 +0530

    feat: user sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,27 @@ import Hero from "@/components/landing/Hero";
 import HowItWorks from "@/components/landing/HowItWorks";
 import PricingSections from "@/components/landing/PricingSections";
 import WhatToAsk from "@/components/landing/WhatToAsk";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+
+
+  const user = await currentUser();
+
+  //redirect auth user to dashboard
+  if (user) redirect("/dashboard")
+
+
   return (
     <div className="min-h-screen bg-background">
       <Header />
-      <Hero/>
-      <HowItWorks/>
-      <WhatToAsk/>
-      <PricingSections/>
-      <CTA/>
-      <Footer/>
+      <Hero />
+      <HowItWorks />
+      <WhatToAsk />
+      <PricingSections />
+      <CTA />
+      <Footer />
     </div>
   );
 }
